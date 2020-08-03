b06d6b423c69f0f49fa8772feb977b7de90e92da
Lucas Iori <liori@aktienow.com>
2020-08-03 11:41:49 -0300

    Primeiro dia: Acelerando a sua evolucao

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img 
+          src="https://avatars3.githubusercontent.com/u/52385902?s=460&u=d7a927755356af72860d07e081419742f8664044&v=4" 
+          alt="Lucas Iori" 
+        />
+        
+        <div>
+          <strong>Lucas Iori</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br /><br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 80,00</strong>
+        </p>
+
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp"/>
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
